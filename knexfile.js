be51728c6c8025aacc6642c82b6799cfb5b2ee16
Chris Daley <Daleys@Chriss-iMac.local>
2020-02-28 09:46:04 -0700

    added a production enviornment is my knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
 
@@ -35,20 +36,16 @@ module.exports = {
     },
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: dbConnection,
+    useNullAsDefault: true,
+    migrations: {
+      directory: '.database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  }
 
 };
