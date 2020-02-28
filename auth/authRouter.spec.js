ab0b3246be1e149543f6eb10f408dfa6c4c4196f
Chris Daley <Daleys@Chriss-iMac.local>
2020-02-28 00:00:44 -0700

    tested Login end point and got a passing test as well

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -1,8 +1,19 @@
 const request = require('supertest');
 const server = require('../api/server');
 const db = require('../database/dbconfig');
+const Parent = require('../parents/parents-model');
+const bcrypt = require('bcryptjs');
 
 // describe.only() { will only run one test =) }
+async function createUser(email, password) {
+  const parent = {
+      email: email,
+      password: bcrypt.hashSync(password, 8)
+  };
+
+  await Parent.add(parent)
+}
+
 
 describe('should reigster a new user', () => {
   it('should return a JSON', async () => {
@@ -16,6 +27,19 @@ describe('should reigster a new user', () => {
   })
 });
 
+describe('POST /login', () => {
+  it('should accept valid credentials', async () => {
+      await createUser("Hulk","smash");
+      const res = await request(server).post('/api/auth/login')
+      .send({
+          "email": "Hulk",
+          "password": "smash"
+      });
+      console.log(res.body);
+      expect(res.type).toEqual('application/json');
+      expect(res.status).toEqual(200);
+  });
+});
 
 beforeEach( async () => {
   await db ('parents').truncate();
