43597a5b7a58e517248e8978fdcf448d7ae3dcaf
Chris Daley <Daleys@Chriss-iMac.local>
2020-03-03 23:45:14 -0700

    added a delete request to my parents-routers. it works in Post man

--- parents/parents-router.js
+++ parents/parents-router.js
@@ -1,32 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Parents = require('./parents-model');
+const Parents = require("./parents-model");
 
-const authorized = require('../auth/authenticate-middleware');
+const authorized = require("../auth/authenticate-middleware");
 
-router.get('/', authorized, ( req,res )=> {
+router.get("/", authorized, (req, res) => {
   Parents.find()
     .then(parents => {
       res.json(parents);
     })
-    .catch(err => res.send({ message:
-        "cannot retrieve parents", err }
-  ));
+    .catch(err => res.send({ message: "cannot retrieve parents", err }));
 });
 
-router.get('/:id', authorized, (req,res) => {
+router.get("/:id", authorized, (req, res) => {
   const { id } = req.params;
-console.log(id);
+  console.log(id);
   Parents.findById(id)
     .then(parents => {
       res.json(parents);
     })
-    .catch(err => res.send({ message:
-    "cannot retrieve that person", err }
-  ));
+    .catch(err => res.send({ message: "cannot retrieve that person", err }));
 });
 
-router.put('/:id', authorized, async (req, res) => {
+router.put("/:id", authorized, async (req, res) => {
   const { id } = req.params;
   const data = req.body;
 
@@ -34,17 +30,34 @@ router.put('/:id', authorized, async (req, res) => {
     const changed = await Parents.findById(id);
 
     if (changed) {
-        Parents.update(data, id)
-            .then(updatedUser => {
-                res.status(200).json({ message: 'Updated the user!', data });
-            });
+      Parents.update(data, id).then(updatedUser => {
+        res.status(200).json({ message: "Updated the user!", data });
+      });
     } else {
-        console.log(id);
-        res.status(404).json({ message: 'Could not find user.' })
+      console.log(id);
+      res.status(404).json({ message: "Could not find parent." });
     }
   } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: 'Failed to update user, try again later.', err })
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Failed to update parent, try again later.", err });
+  }
+});
+
+router.delete("/:id", authorized, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleted = await Parents.remove(id);
+    res.status(200).json({ message: `Successfully removed the parent ${id}.` });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        message: "Could not remove this parent, please try again later.",
+        err
+      });
   }
 });
 
