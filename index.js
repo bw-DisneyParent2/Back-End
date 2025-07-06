22fe6770138a3a405ae715aac670b189c46924ec
Carlos Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2025-07-06 18:13:18 -0400

    Switch to programmatic knex migrations

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 require("dotenv").config();
+const environment = process.env.DB_ENV || "development";
 
-const server = require('./api/server.js');
+if (environment === "production") {
+  const knex = require("knex")(require("./knexfile.js").production);
 
-const PORT = process.env.PORT || 3500;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
+  knex.migrate.latest()
+    .then(() => {
+      console.log("Migrations completed");
+      startServer();
+    })
+    .catch(err => {
+      console.error("Migration failed", err);
+      process.exit(1);
+    });
+} else {
+  startServer();
+}
+
+
+function startServer() {
+  const server = require('./api/server.js');
+
+  const PORT = process.env.PORT || 3500;
+  server.listen(PORT, () => {
+    console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  });
+}
+
