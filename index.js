7706f1eadd72113fffb1c7d1f85ae2d4a9ef854c
Carlos Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2025-07-06 18:15:32 -0400

    Switch back to regular index.js

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 require("dotenv").config();
-const environment = process.env.DB_ENV || "development";
 
-if (environment === "production") {
-  const knex = require("knex")(require("./knexfile.js").production);
+const server = require('./api/server.js');
 
-  knex.migrate.latest()
-    .then(() => {
-      console.log("Migrations completed");
-      startServer();
-    })
-    .catch(err => {
-      console.error("Migration failed", err);
-      process.exit(1);
-    });
-} else {
-  startServer();
-}
-
-
-function startServer() {
-  const server = require('./api/server.js');
-
-  const PORT = process.env.PORT || 3500;
-  server.listen(PORT, () => {
-    console.log(`\n=== Server listening on port ${PORT} ===\n`);
-  });
-}
-
+const PORT = process.env.PORT || 3500;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
