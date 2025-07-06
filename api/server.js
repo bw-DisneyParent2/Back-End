c02e2d0a4e2ccd0f4f5b7ac28bbaa296f93e76f2
Carlos Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2025-07-06 10:09:28 -0400

    Fix: Downgraded connect-session-knex to connect-session-knex@3 for compatibility with CommonJS

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const jwt = require('jsonwebtoken');
 const session = require("express-session");
 const sessionStore = require("connect-session-knex")(session);
+const knexConfig = require("../database/dbconfig");
 
 const authRouter = require("../auth/auth-router");
 const parentsRouter = require("../parents/parents-router");
@@ -11,53 +12,53 @@ const parentsRouter = require("../parents/parents-router");
 const server = express();
 
 const sessionOptions = {
-    name: "ItsDangerousOut",
-    secret: "TakeThis",
-    cookie: {
-      maxAge: 1000 * 60 * 30,
-      secure: false,
-      httpOnly: true
-    },
-    resave: false,
-    saveUninitialized: false,
-  
-    store: new sessionStore({
-      knex: require("../database/dbconfig"),
-      tablename: "sessions",
-      sidfieldname: "sid",
-      createtable: true,
-      clearInterval: 1000 * 60 * 30
-    })
-    };
+  name: "ItsDangerousOut",
+  secret: "TakeThis",
+  cookie: {
+    maxAge: 1000 * 60 * 30,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
 
-  server.use(helmet());
-  server.use(cors());
-  server.use(express.json());
-  
-  server.get('/', (req, res) => {
-    res.send("Backend for Disney Parents 2");
-  }); 
-  
-  server.use(session(sessionOptions));
-  server.use('/api/auth', authRouter);
-  server.use('/api/parents', parentsRouter);
+  store: new sessionStore({
+    knex: knexConfig,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30
+  })
+};
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send({ api_status: "up", message: "Backend for Disney Parents 2" });
+});
+
+server.use(session(sessionOptions));
+server.use('/api/auth', authRouter);
+server.use('/api/parents', parentsRouter);
 
 server.get('/token', (req, res) => {
 
-    const payload = {
-      subject: 'thisuser',
-      parentid: 'parent.id',
-      favoriteChili: 'red hot chili peppers'
-    };
+  const payload = {
+    subject: 'thisuser',
+    parentid: 'parent.id',
+    favoriteChili: 'red hot chili peppers'
+  };
 
-    const secret = 'takethis';
-    const options = {
-      expiresIn: '1h'
-    };
+  const secret = 'takethis';
+  const options = {
+    expiresIn: '1h'
+  };
 
-    const token = jwt.sign(payload, secret, options);
+  const token = jwt.sign(payload, secret, options);
 
-    res.json(token);
+  res.json(token);
 });
 
 
